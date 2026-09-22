48060d98bb58b663e4dac8f4794a9bb8d3a5f138
milkaodich-beep <milkaodich@gmail.com>
2026-09-22 15:42:32 +0300

    Add Alembic environment

---
+++ backend/alembic/env.py
@@ -0,0 +1,30 @@
+import asyncio
+from logging.config import fileConfig
+from alembic import context
+from sqlalchemy import Connection, pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from app import models
+from app.db import Base, settings
+
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+def run_migrations_offline():
+    context.configure(url=settings.database_url, target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
+    with context.begin_transaction(): context.run_migrations()
+
+def do_run_migrations(connection: Connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction(): context.run_migrations()
+
+async def run_migrations_online():
+    connectable = async_engine_from_config({"sqlalchemy.url": settings.database_url}, prefix="sqlalchemy.", poolclass=pool.NullPool)
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+    await connectable.dispose()
+
+if context.is_offline_mode(): run_migrations_offline()
+else: asyncio.run(run_migrations_online())
